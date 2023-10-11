28bc5def394d82a6686dc5d5ab35c3efa39f78f3
housekjh1 <housekjh@gmail.com>
2023-10-11 16:01:34 +0900

    1011
    - 스프링 부트 학습내용 초기업로드

---
+++ Chapter01/src/main/java/com/rubypaper/controller/BoardController.java
@@ -0,0 +1,55 @@
+package com.rubypaper.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+class Person {
+	private String name;
+	private Integer age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+
+}
+
+@RestController
+public class BoardController {
+	public BoardController() {
+		System.out.println("===> BoardController 생성");
+	}
+
+	@GetMapping("/hello")
+	public String hello(String name) {
+		return "Hello : " + name;
+	}
+
+	@GetMapping("/hello1")
+	public String hello(String name, Integer age) {
+		if (age == null)
+			age = 0;
+		return "Hello : " + name + "(" + age + "세)";
+	}
+
+	@GetMapping("/person")
+	public String person(Person person) {
+		return person.toString();
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# workspace-sts4
+
+> 스프링 부트 학습용 레포지터리입니다.
+
+## 환경
+https://spring.io/tools
