34c0eb1d08a82683d5f1630a27d7b61a170ada81
housekjh1 <housekjh@gmail.com>
2023-10-12 14:39:05 +0900

    1012
    - lombok 설치방법 README.md 업데이트

--- Mission0/src/main/java/edu/pnu/controller/MemberController.java
+++ Mission0/src/main/java/edu/pnu/controller/MemberController.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.pnu.domain.MemberVO;
 
 @RestController
 public class MemberController {
+
 	List<MemberVO> list;
 
 	public MemberController() {
@@ -56,6 +59,15 @@ public class MemberController {
 			list.add(m);
 			return list.get(m.getId());
 		}
+	}
 
+	@PutMapping("/member")
+	public MemberVO updateMembers(MemberVO m) {
+		return null;
+	}
+
+	@DeleteMapping("/member/{id}")
+	public MemberVO removeMember(@PathVariable Integer id) {
+		return null;
 	}
 }
--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 ## 환경
 https://spring.io/tools
+
+lombok 추가 : sts4 설치 위치에 설치, 프로젝트 우클릭 - Maven - Update Project
+https://mvnrepository.com/artifact/org.projectlombok/lombok/1.18.30
